a3e37b1e639e71c648f542bdfd02893f4fc8a051
kantapong_int <kantapong.int.work@gmail.com>
2024-05-24 11:33:18 +0700

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import "./App.css";
 import r2wc from "react-to-webcomponent";
 import PropTypes from 'prop-types';
 
-const HelloApp = ({ name }) => {
+const Greeting = ({ name }) => {
   return <h1>Hello, {name}!</h1>
 }
 
-const WebGreeting = r2wc(HelloApp,{
-  props:{name:'string'}
-});
+const WebGreeting = r2wc(Greeting, {
+  props: {
+    name: "string",
+  },
+})
 
 customElements.define("web-greeting", WebGreeting);
 
-export default HelloApp
+export default Greeting
 
 
-HelloApp.propTypes = {
+Greeting.propTypes = {
   name: PropTypes.string
 }
