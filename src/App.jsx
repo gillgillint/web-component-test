60bccece4b60ad2020aaf185bca40610e0c6a5a8
kantapong_int <kantapong.int.work@gmail.com>
2024-05-24 13:09:09 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import "./App.css";
+import r2wc from "@r2wc/react-to-web-component"
 
 
-import r2wc from "react-to-webcomponent";
-import PropTypes from 'prop-types';
+const Greeting = () => {
+  return <h1>Hello, World!</h1>
+}
+const WebGreeting = r2wc(Greeting)
 
-const Greeting = ({ name }) => {
-  return <h1>Hello, {name}!</h1>
+customElements.define("web-greeting", WebGreeting)
+
+function App() {
+  return <></>;
 }
 
-const WebGreeting = r2wc(Greeting, {
-  props: {
-    name: "string",
-  },
-})
-
-customElements.define("web-greeting", WebGreeting);
-
-export default Greeting
-
-
-Greeting.propTypes = {
-  name: PropTypes.string
-}
+export default App;
